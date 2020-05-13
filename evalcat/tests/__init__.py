e1b48ab491b0c8c2223b2e79447abb2a66a01bab
Muraoka Masaki <muraokamasaki96@gmail.com>
2020-05-13 16:35:04 +0900

    Fixed bug where tests could not be discovered.

--- evalcat/tests/__init__.py
+++ evalcat/tests/__init__.py
@@ -1,8 +1,10 @@
+import os
 import unittest
 
 
 def test():
+    test_dir = os.path.dirname(__file__)
     loader = unittest.TestLoader()
-    suite = loader.discover('.')
+    suite = loader.discover(test_dir)
 
     unittest.TextTestRunner().run(suite)
